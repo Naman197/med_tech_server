fdaf6cdd71b966aaec5abd18ec6db00f08565ffb
Naman197 <modinaman5@gmail.com>
2024-09-01 12:51:13 +0530

    backend intial14

--- server.js
+++ server.js
@@ -89,6 +89,7 @@ const express = require('express');
    }));
 
    app.use(express.json());
+   app.use(express.urlencoded({ extended: true }));
 
 
    app.use('/api/manufacturers/inv',manfinvroute );
