36a966807405b8de02e2b13658961b08c355452c
Naman197 <modinaman5@gmail.com>
2024-09-04 15:42:51 +0530

    backend intial40

--- models/manufacturers/qrCode.js
+++ models/manufacturers/qrCode.js
@@ -3,17 +3,17 @@ const Schema = mongoose.Schema;
 
 // Define the QR Code Schema
 const QRCodeSchema = new Schema({
-  qrCode: { type: String, unique: true, required: true }, // QR code value
-  location: { type: String, required: true }, // Location where the QR code is used
-  number: { type: String, required: true }, // QR code number or identifier
+  qrCode: { type: String, unique: true }, // QR code value
+  location: { type: String }, // Location where the QR code is used
+  number: { type: String }, // QR code number or identifier
   condition: { 
     type: String,
     enum: ['New', 'Used', 'Damaged', 'Pending'], // Conditions of the QR code
     default: 'Pending' // Default condition is 'Pending' before finalizing
   },
-  orderId: { type: Schema.Types.ObjectId, ref: 'Order', required: true }, // Reference to an Order
-  distributorId: { type: Schema.Types.ObjectId, ref: 'Distributor', required: true }, // Reference to a Distributor
-  manufacturerId: { type: Schema.Types.ObjectId, ref: 'Manufacturer', required: true },
+  orderId: { type: Schema.Types.ObjectId, ref: 'Order' }, // Reference to an Order
+  distributorId: { type: Schema.Types.ObjectId, ref: 'Distributor' }, // Reference to a Distributor
+  manufacturerId: { type: Schema.Types.ObjectId, ref: 'Manufacturer' },
   qrCodeUrl: { type: String }, // URL of the generated QR code image
   accessDate: { type: Date }, // Date when the QR code was accessed
   boxImage: { type: String }, // URL of the box image
