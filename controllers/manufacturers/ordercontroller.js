22aa77d836982855795315fa04d38cecb07065a2
Naman197 <modinaman5@gmail.com>
2024-09-05 21:37:20 +0530

    backend intial48

--- controllers/manufacturers/ordercontroller.js
+++ controllers/manufacturers/ordercontroller.js
@@ -260,7 +260,7 @@ const getOrdersByManufacturer = async (req, res) => {
         })
         .populate('manufacturer.manufacturerId', 'name')
         .populate({
-            path: 'medicines', // Populate from ManufacturerProduct
+            path: 'medicines.manufacturerId', // Populate from ManufacturerProduct
             select: 'name batchNo mrp cost sellingPrice productionDate expiryDate composition temperature'
         });
 
